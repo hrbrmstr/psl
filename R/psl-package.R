8d7b1d251249a908967f06e34acf56b1caac5d9e
hrbrmstr <bob@rud.is>
2018-09-06 08:27:33 -0400

    R package repo initialization complete

---
+++ R/psl-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/psl>
+#' - BugReports: <https://gitlab.com/hrbrmstr/psl/issues>
+#' 
+#' @md
+#' @name psl
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
